825d0c63a2064d23a1e530c8fb5e582d0068b31b
Lucas Greenwell <lucasgreenwell15@gmail.com>
2019-12-03 15:19:45 -0600

    declared all my lists and have started adding eventlisteners to each

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
 // Your code goes here
+
+//nodelist of the four navlinks
+const navLinks = document.querySelectorAll(".nav-link");
+
+//creates hoverlike responses for the navlinks
+for (let i = 0; i < navLinks.length; i ++){
+    let link = navLinks[i];
+    link.addEventListener("mouseenter", () => {
+        link.style.color = "red";
+        link.style.fontSize = "2rem";
+    })
+    link.addEventListener("mouseout", () => {
+        link.style.color = "black";
+        link.style.fontSize = "1.6rem";
+    })
+}
+
+//nodelist of three buttons
+const buttons = document.querySelectorAll(".btn");
+
+//creates multiple color states for the buttons
+for (let i = 0; i < buttons.length; i ++){
+    let button = buttons[i];
+    button.addEventListener("click", () => {
+        button.style.backgroundColor = "#212529";
+        button.style.color = "#17A2B8";
+    })
+    button.addEventListener("dblclick", () => {
+        button.style.backgroundColor = "#746";
+        button.style.color = "#123";
+    })
+}
+
+const textContent = document.querySelectorAll(".text-content");
+// console.log(textContent);
+//nodelist of two textareas
+
+const imgContent = document.querySelectorAll(".img-content");
+// console.log(imgContent);
+//nodelist of two images
+
+const destinations = document.querySelectorAll(".destination");
+// console.log(destinations);
+//nodelist of three destination cards
+
+
+
+
+
+
